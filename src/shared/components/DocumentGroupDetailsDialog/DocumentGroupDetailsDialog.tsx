6c3f08f56695e93764366d42fc353fc6cd17a548
Сергей Селезнев <seleznev@echo-company.ru>
2023-03-23 09:12:26 +0500

    feat: group createng and editing

--- src/shared/components/DocumentGroupDetailsDialog/DocumentGroupDetailsDialog.tsx
+++ src/shared/components/DocumentGroupDetailsDialog/DocumentGroupDetailsDialog.tsx
@@ -3,30 +3,27 @@ import React, { useEffect } from "react";
 import SaveIcon from "@mui/icons-material/Save";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Controller, useForm } from "react-hook-form";
-import { LinkedDocument, LinkedDocumentInput } from "~/generated/graphql";
+import { DocumentGroup, DocumentGroupInput } from "~/generated/graphql";
 import { getErrorMessage } from "~/shared/lib/getError";
 import { baseRequired } from "~/shared/lib/validation";
-import { getFileFormat } from "~/shared/lib/getFileFormat";
 import { Button } from "../Button";
 import { HelperText } from "../HelperText";
 import { RequiredLabelWrapper } from "../RequiredLabelWrapper";
 import { Text } from "../Text";
-import { FileInput } from "../FileInput";
-import { getFileName } from "../../lib/getFileName";
 
 type Props = {
   open: boolean;
   onClose: () => void;
-  create?: (document: Omit<LinkedDocumentInput, "id">) => void;
-  update?: (document: LinkedDocumentInput) => void;
-  onRemove?: (id: LinkedDocumentInput["id"]) => void;
-  document?: LinkedDocument | null;
+  create?: (group: Omit<DocumentGroupInput, "id">) => void;
+  update?: (group: DocumentGroupInput) => void;
+  onRemove?: (id: DocumentGroupInput["id"]) => void;
+  group?: DocumentGroup | null;
 };
 
-export const DocumentDetailsDialog: React.FC<Props> = ({
+export const DocumentGroupDetailsDialog: React.FC<Props> = ({
   open,
   onClose,
-  document,
+  group,
   onRemove,
   create,
   update
@@ -36,19 +33,17 @@ export const DocumentDetailsDialog: React.FC<Props> = ({
     control,
     setValue,
     formState: { errors },
-    handleSubmit,
-    getValues
+    handleSubmit
   } = useForm({ mode: "all" });
 
   const getError = getErrorMessage(errors);
 
-  const isCreate = !document?.id;
+  const isCreate = !group?.id;
 
   const onSubmit = handleSubmit((newValues) => {
     const input = {
-      ...(Boolean(isCreate) && { id: document?.id }),
-      ...(Boolean(newValues.file) && { upload: newValues.file }),
-      user_name: `${newValues.title}.${newValues.format}`
+      ...(Boolean(isCreate) && { id: group?.id }),
+      name: newValues.name
     };
 
     if (isCreate) {
@@ -60,72 +55,41 @@ export const DocumentDetailsDialog: React.FC<Props> = ({
   });
 
   const handleDelete = () => {
-    if (document?.id) {
-      onRemove?.(document.id);
+    if (group?.id) {
+      onRemove?.(group.id);
     }
 
     onClose();
   };
 
   useEffect(() => {
-    setValue("title", getFileName(document?.user_name ?? ""));
-    setValue("url", document?.url);
-    setValue("format", getFileFormat(document?.user_name ?? ""));
-  }, [document, setValue]);
+    setValue("name", group?.name);
+  }, [group, setValue]);
 
   return (
     <Drawer anchor='right' open={open} onClose={onClose}>
       <Box className='flex flex-col gap-10 p-6' component='form' onSubmitCapture={onSubmit}>
-        <Text variant='h5'>{document ? "Edit document" : "Create document"}</Text>
+        <Text variant='h5'>{document ? "Edit group" : "Create group"}</Text>
 
         <Controller
           control={control}
-          name='title'
+          name='name'
           render={({ field }) => (
             <FormControl fullWidth>
               <TextField
                 fullWidth
                 label={
                   <RequiredLabelWrapper>
-                    <Text>Title</Text>
+                    <Text>Name</Text>
                   </RequiredLabelWrapper>
                 }
                 {...field}
-                error={!!getError("title")}
-                {...register("title", baseRequired)}
+                error={!!getError("name")}
+                {...register("name", baseRequired)}
               />
 
-              <HelperText id='title' error={getError("title")} />
+              <HelperText id='name' error={getError("name")} />
             </FormControl>
-          )}
-        />
-
-        <Controller
-          control={control}
-          name='url'
-          render={({ field: { value } }) => (
-            <FileInput
-              id='file-input'
-              url={value}
-              fileName={getValues("title")}
-              onFileChange={(file) => {
-                setValue("file", file);
-
-                const format = getFileFormat(file?.name ?? "");
-
-                setValue("format", format);
-                setValue("title", file?.name ?? "");
-              }}
-              onUrlChange={(url = "") => {
-                setValue("url", url);
-              }}
-              onDelete={() => {
-                setValue("url", "");
-                setValue("file", null);
-                setValue("title", "");
-                setValue("format", "");
-              }}
-            />
           )}
         />
 
