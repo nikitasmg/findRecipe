5e2aae51a5dbf40d34ecc3bdae2c52b708c6ab98
Сергей Селезнев <seleznev@echo-company.ru>
2023-03-27 03:48:49 +0500

    fix: organizers and partners crud

--- src/modules/EventsDetailsForm/components/Member/Member.tsx
+++ src/modules/EventsDetailsForm/components/Member/Member.tsx
@@ -3,33 +3,67 @@ import AddIcon from "@mui/icons-material/Add";
 import BackspaceIcon from "@mui/icons-material/Backspace";
 import { Controller, useForm } from "react-hook-form";
 import { Box, Drawer, TextField, Typography } from "@mui/material";
+import { useGraphqlClient } from "~/app/providers/GraphqlClient";
 import { AvatarInput } from "~/shared/components/AvatarInput";
 import { Button } from "~/shared/components/Button";
 import { Text } from "~/shared/components/Text";
 import { SaveButton } from "~/shared/components/SaveButton";
 import { useModal } from "~/shared/hooks/useModal";
 import { useAlertsStore } from "~/shared/stores/alerts";
+import { getMemberHooksByName } from "../../lib/getMemberHooksByName";
+import { ButtonDelete } from "~/shared/components/ButtonDelete";
+import { compose, not, prop, when } from "rambda";
+import {
+  CreateOrganizerMutation,
+  CreatePartnerMutation,
+  OrganizerInput,
+  PartnerInput
+} from "~/generated/graphql";
+import { equals } from "ramda";
+
+type UpsertInput = OrganizerInput | PartnerInput;
+
+type DeleteInput = { id: number };
+
+type CreateMutation =
+  | CreateOrganizerMutation["createOrganizer"]
+  | CreatePartnerMutation["createPartner"];
 
 type FormFields = {
   name: string;
-  avatarUrl: string;
+  imageUrl?: string | null;
   file: File | null;
+  deleteImage?: boolean;
 };
 
 type Member = {
+  id?: number;
   name: string;
-  avatarUrl: string;
+  imageUrl?: string | null;
   file?: File | null;
 };
 
 type Props = {
   title: string;
   attachTitle: string;
+  memberType: "organizer" | "partner";
   value?: Member[];
   onChange?: (members: Member[]) => void;
+  onCreateMember?: (input: UpsertInput) => void;
+  onUpdateMember?: (input: UpsertInput) => void;
+  onDeleteMember?: (input: DeleteInput) => void;
 };
 
-export const Member: React.FC<Props> = ({ title, attachTitle, value = [], onChange }) => {
+export const Member: React.FC<Props> = ({
+  title,
+  attachTitle,
+  memberType,
+  value = [],
+  onChange,
+  onCreateMember,
+  onUpdateMember,
+  onDeleteMember
+}) => {
   const [members, setMembers] = useState(value);
 
   const [selectedMember, setSelectedMember] = useState<Member | null>();
@@ -40,11 +74,46 @@ export const Member: React.FC<Props> = ({ title, attachTitle, value = [], onChan
 
   const addAlert = useAlertsStore((state) => state.addAlert);
 
-  useEffect(() => {
-    setMembers(value);
-  }, [value]);
+  const client = useGraphqlClient();
 
-  const handleOpenForm = () => {
+  const { useCreate, useUpdate, useRemove } = getMemberHooksByName(memberType);
+
+  const { mutateAsync: create } = useCreate(client);
+
+  const { mutateAsync: update } = useUpdate(client);
+
+  const { mutateAsync: remove } = useRemove(client);
+
+  const handleRemove = () => {
+    when(
+      Boolean,
+      compose((id) => {
+        const input = { id: id as number };
+        remove(input);
+        onDeleteMember?.(input);
+        setMembers((oldMembers) => {
+          const newMembers = oldMembers.filter(
+            compose(not, equals(selectedMember?.id), prop("id"))
+          );
+
+          onChange?.(newMembers);
+
+          return newMembers;
+        });
+      }, prop("id")),
+      selectedMember
+    );
+
+    handleClose();
+  };
+
+  const getOpenFormHandler = (member: Member) => () => {
+    reset();
+    setSelectedMember(member);
+    handleOpen();
+  };
+
+  const handleAddOpen = () => {
     reset();
     setSelectedMember(null);
     handleOpen();
@@ -60,25 +129,70 @@ export const Member: React.FC<Props> = ({ title, attachTitle, value = [], onChan
     e.preventDefault();
     e.stopPropagation();
 
-    handleSubmit((values) => {
-      if (selectedMember) {
-        setMembers((oldMembers) => {
-          const newFiles = oldMembers.reduce((res: Member[], cur) => {
-            res.push(cur === selectedMember ? values : cur);
+    handleSubmit((newValues) => {
+      handleCloseForm();
 
-            return res;
-          }, []);
+      const input = {
+        ...(Boolean(selectedMember?.id) && { id: selectedMember?.id }),
+        name: newValues.name,
+        ...(Boolean(newValues.file) && { uploadImage: newValues.file }),
+        ...(Boolean(newValues.deleteImage) && { deleteImage: newValues.deleteImage })
+      };
 
-          onChange?.(newFiles);
-          return newFiles;
+      if (!selectedMember) {
+        create({ input }).then((data) => {
+          const mutationKey = Object.keys(data)[0];
+
+          const values: CreateMutation = data[mutationKey as keyof typeof data] as CreateMutation;
+
+          if (!values) {
+            return;
+          }
+
+          onCreateMember?.(values);
+
+          setMembers((oldMembers) => {
+            const newMembers = oldMembers.concat(values);
+
+            onChange?.(newMembers);
+
+            return newMembers;
+          });
         });
-      } else {
-        setMembers((oldMembers) => oldMembers.concat(values));
+
+        return;
       }
 
-      handleCloseForm();
+      setMembers((oldMembers) => {
+        const newMembers = oldMembers.reduce((res: Member[], cur) => {
+          res.push(cur === selectedMember ? newValues : cur);
+
+          return res;
+        }, []);
+
+        onChange?.(newMembers);
+
+        return newMembers;
+      });
+
+      update({ input });
+
+      onUpdateMember?.({ ...selectedMember, ...newValues });
     })(e);
   };
+
+  useEffect(() => {
+    setMembers(value);
+  }, [value]);
+
+  useEffect(() => {
+    if (!selectedMember) {
+      return;
+    }
+
+    setValue("imageUrl", selectedMember.imageUrl);
+    setValue("name", selectedMember.name);
+  }, [selectedMember, setValue]);
 
   return (
     <Fragment>
@@ -87,10 +201,16 @@ export const Member: React.FC<Props> = ({ title, attachTitle, value = [], onChan
       <Box className='flex flex-col gap-4'>
         <Box className='flex flex-wrap gap-6'>
           {members.map((member, i) => (
-            <Box className='flex-1 min-w-[200px] shrink-0' key={i}>
+            <Box
+              className='flex-1 min-w-[200px] shrink-0'
+              key={i}
+              onClick={getOpenFormHandler(member)}
+              tabIndex={0}
+              onKeyPress={getOpenFormHandler(member)}
+            >
               <img
                 className='rounded-full w-[60px] h-[60px]'
-                src={member.avatarUrl}
+                src={member.imageUrl ?? ""}
                 alt={member.name}
               />
               <Typography>{member.name}</Typography>
@@ -99,7 +219,7 @@ export const Member: React.FC<Props> = ({ title, attachTitle, value = [], onChan
         </Box>
 
         <Button
-          onClick={handleOpenForm}
+          onClick={handleAddOpen}
           className='bg-gray-200 hover:bg-gray-300 w-[60px] h-[60px] p-0 min-w-[40px] rounded-full'
         >
           <AddIcon />
@@ -112,19 +232,20 @@ export const Member: React.FC<Props> = ({ title, attachTitle, value = [], onChan
 
           <Controller
             control={control}
-            name='avatarUrl'
+            name='imageUrl'
             render={({ field: { value } }) => (
               <AvatarInput
                 addAlert={addAlert}
                 id='file-input'
-                url={value}
+                url={value ?? ""}
                 onChange={(file) => {
                   setValue("file", file as File);
-                  setValue("avatarUrl", URL.createObjectURL(file as File));
+                  setValue("imageUrl", URL.createObjectURL(file as File));
                 }}
                 onDelete={() => {
                   setValue("file", null);
-                  setValue("avatarUrl", "");
+                  setValue("imageUrl", "");
+                  setValue("deleteImage", true);
                 }}
               />
             )}
@@ -145,16 +266,20 @@ export const Member: React.FC<Props> = ({ title, attachTitle, value = [], onChan
           />
 
           <Box className='flex gap-2'>
-            <Button
-              className='flex-1'
-              color='error'
-              type='button'
-              variant='outlined'
-              startIcon={<BackspaceIcon />}
-              onClick={handleCloseForm}
-            >
-              Back
-            </Button>
+            {!selectedMember && (
+              <Button
+                className='flex-1'
+                color='error'
+                type='button'
+                variant='outlined'
+                startIcon={<BackspaceIcon />}
+                onClick={handleCloseForm}
+              >
+                Back
+              </Button>
+            )}
+
+            {selectedMember && <ButtonDelete className='flex-1' onClick={handleRemove} />}
 
             <SaveButton className='flex-1' />
           </Box>
