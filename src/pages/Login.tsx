ad99af98dde1ccc14b65e10c50f70e6937de8512
Сергей Селезнев <seleznev@echo-company.ru>
2023-03-28 15:47:16 +0500

    feat: Set logos

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,11 +8,16 @@ export const Login: React.FC = () => {
   return (
     <Fragment>
       <Container className='!flex flex-col items-center justify-center h-full w-full'>
-        <Grid container spacing={6}>
-          <Grid item columns={12} xs={12} className='flex items-center justify-center'>
+        <Grid container spacing={4}>
+          <Grid
+            item
+            columns={12}
+            xs={12}
+            className='flex items-center flex-col gap-2 justify-center'
+          >
             <Logo size='big' />
-            <Text className='pl-2' variant='h2' component='h1'>
-              Admin Panel
+            <Text className='pl-2 text-4xl' align='center' component='h1'>
+              Administration system
             </Text>
           </Grid>
           <Grid item columns={12} xs={12} className='flex items-center justify-center'>
