62ca2cbc122f24ae2ec37deb1ce4cbfd7a6b98b0
Сергей Селезнев <seleznev@echo-company.ru>
2023-03-28 14:07:54 +0500

    init project

--- scripts/changelog-update.sh
+++ scripts/changelog-update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git log --no-merges --pretty=" - <ins>%as</ins> **[\`%h\`](https://gitlab.dev.echo-company.ru/developers/echo-front-v3/-/commit/%H)** %s" > CHANGELOG.md
+git log --no-merges --pretty=" - <ins>%as</ins> **[\`%h\`](https://gitlab.dev.echo-company.ru/yugra/frontend/yugra-admin/-/commit/%H)** %s" > CHANGELOG.md
