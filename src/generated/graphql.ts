10abfe040bd42bd6821f6bc7648af636ce963673
Сергей Селезнев <seleznev@echo-company.ru>
2023-03-30 04:24:16 +0500

    feat: edit news data by lang

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -2254,15 +2254,6 @@ export type AllNewsQueryVariables = Exact<{
   filter?: InputMaybe<Array<FilterByClause> | FilterByClause>;
 }>;
 
-
-export type AllNewsQuery = { __typename?: 'Query', allNewsIds?: { __typename?: 'NewsPaginator', data: Array<{ __typename?: 'News', id: number }> } | null };
-
-export type AllNewsQueryVariables = Exact<{
-  orderBy?: InputMaybe<Array<OrderByClause> | OrderByClause>;
-  filter?: InputMaybe<Array<FilterByClause> | FilterByClause>;
-}>;
-
-
 export type AllNewsQuery = { __typename?: 'Query', allNewsIds?: { __typename?: 'NewsPaginator', data: Array<{ __typename?: 'News', id: number }> } | null };
 
 export type UpdateOnIndexMutationVariables = Exact<{
