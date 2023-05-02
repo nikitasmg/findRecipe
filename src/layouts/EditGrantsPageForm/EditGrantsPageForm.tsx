2ac89258d81016f75a7198fdc54d64f802de83b3
SadreevArtem <sadreev@echo-company.ru>
2023-05-02 10:08:51 +0500

    addInfoBlock

--- src/layouts/EditGrantsPageForm/EditGrantsPageForm.tsx
+++ src/layouts/EditGrantsPageForm/EditGrantsPageForm.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import { PageForm } from "~/modules/PageForm";
 
 export const EditGrantsPageForm: React.FC = () => {
-  return <PageForm slug='grants' isDocumentsExist />;
+  return <PageForm slug='grants' isDocumentsExist isInfoBlockCards />;
 };
