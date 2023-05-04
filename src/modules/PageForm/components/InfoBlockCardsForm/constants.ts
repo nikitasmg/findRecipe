cbab7a1743fb8285fad36c66bbb03885967cc230
SadreevArtem <sadreev@echo-company.ru>
2023-05-04 11:16:06 +0500

    modifiedInfoBlockCards

--- src/modules/PageForm/components/InfoBlockCardsForm/constants.ts
+++ src/modules/PageForm/components/InfoBlockCardsForm/constants.ts
@@ -2,6 +2,8 @@ export const CARDS_ONE_TITLE = "params.cards_1.title";
 export const CARDS_ONE_DESCRIPTION = "params.cards_1.description";
 export const CARDS_ONE_TITLE_EN = "params.cards_1.title_en";
 export const CARDS_ONE_DESCRIPTION_EN = "params.cards_1.description_en";
+export const CARDS_ONE_BUTTON_NAME = "params.cards_1.button_name";
+export const CARDS_ONE_BUTTON_NAME_EN = "params.cards_1.button_name_en";
 export const CARDS_ONE_IMAGE = "params.cards_1.image";
 export const CARDS_ONE_ROUTE = "params.cards_1.route";
 
@@ -9,5 +11,7 @@ export const CARDS_TWO_TITLE = "params.cards_2.title";
 export const CARDS_TWO_DESCRIPTION = "params.cards_2.description";
 export const CARDS_TWO_TITLE_EN = "params.cards_2.title_en";
 export const CARDS_TWO_DESCRIPTION_EN = "params.cards_2.description_en";
+export const CARDS_TWO_BUTTON_NAME = "params.cards_2.button_name";
+export const CARDS_TWO_BUTTON_NAME_EN = "params.cards_2.button_name_en";
 export const CARDS_TWO_IMAGE = "params.cards_2.image";
 export const CARDS_TWO_ROUTE = "params.cards_2.route";
