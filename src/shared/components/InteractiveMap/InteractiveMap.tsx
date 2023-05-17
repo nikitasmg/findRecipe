eda4571e9b0b673fa57db827f1a5e9adc2a6c67f
SadreevArtem <sadreev@echo-company.ru>
2023-05-17 16:32:57 +0500

    removeQuickPinchZoom

--- src/shared/components/InteractiveMap/InteractiveMap.tsx
+++ src/shared/components/InteractiveMap/InteractiveMap.tsx
@@ -1,52 +1,15 @@
-import Add from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
 import { Box, IconButton } from "@mui/material";
-import React, { useCallback, useRef, useState, MouseEventHandler } from "react";
-import QuickPinchZoom, { make3dTransformValue, UpdateAction } from "react-quick-pinch-zoom";
+import React, { MouseEventHandler } from "react";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { MapSVG } from "../MapSVG";
+import { Add } from "@mui/icons-material";
 
 type Props = {
   onSelect?: (id: string) => void;
 };
 
 export const InteractiveMap: React.FC<Props> = ({ onSelect }) => {
-  const [zoom, setZoom] = useState<UpdateAction>({ x: 0, y: 0, scale: 1 });
-
-  const imgRef = useRef<HTMLDivElement>(null);
-
-  const zoomRef = useRef<QuickPinchZoom>(null);
-
-  const onUpdate = useCallback(({ x, y, scale }: UpdateAction) => {
-    const { current: img } = imgRef;
-
-    setZoom({ x, y, scale });
-
-    if (img) {
-      const value = make3dTransformValue({ x, y, scale });
-
-      img.style.setProperty("transform", value);
-    }
-  }, []);
-
-  const maxScale = 2.5;
-
-  const zoomIn = () => {
-    zoomRef.current?.alignCenter({
-      x: (imgRef.current?.offsetWidth as number) / 2 ?? 0,
-      y: (imgRef.current?.offsetHeight as number) / 2 ?? 0,
-      scale: Math.min((zoom?.scale ?? 1) + 0.5, maxScale)
-    });
-  };
-
-  const zoomOut = () => {
-    if (zoom.scale < 1) return;
-    zoomRef.current?.alignCenter({
-      x: (imgRef.current?.offsetWidth as number) / 2 ?? 0,
-      y: (imgRef.current?.offsetHeight as number) / 2 ?? 0,
-      scale: Math.min((zoom?.scale ?? 1) - 0.5, maxScale)
-    });
-  };
-
   const handleClick: MouseEventHandler<SVGSVGElement> = (e) => {
     const parent = (e.target as Element).parentNode;
 
@@ -60,30 +23,35 @@ export const InteractiveMap: React.FC<Props> = ({ onSelect }) => {
   };
 
   return (
-    <Box className='relative h-fit'>
-      <QuickPinchZoom
-        ref={zoomRef}
-        onUpdate={onUpdate}
-        wheelScaleFactor={500}
-        tapZoomFactor={0.5}
-        maxZoom={maxScale}
-        inertia={true}
-        inertiaFriction={0.96}
-      >
-        <div ref={imgRef}>
-          <MapSVG onClick={handleClick} />
-        </div>
-      </QuickPinchZoom>
+    <Box className='relative w-full'>
+      <div>
+        <TransformWrapper
+          maxScale={2.5}
+          centerZoomedOut
+          centerOnInit
+          disablePadding
+          doubleClick={{ step: 0.7 }}
+          pinch={{ step: 5 }}
+          wheel={{ disabled: true }}
+        >
+          {({ zoomIn, zoomOut }) => (
+            <div className='w-full'>
+              <TransformComponent contentStyle={{ width: "100%" }} wrapperStyle={{ width: "100%" }}>
+                <MapSVG onClick={handleClick} />
+              </TransformComponent>
+              <Box className='absolute bottom-0 right-0 flex flex-col p-4'>
+                <IconButton onClick={() => zoomIn()}>
+                  <Add />
+                </IconButton>
 
-      <Box className='absolute bottom-0 right-0 flex flex-col p-4'>
-        <IconButton onClick={zoomIn}>
-          <Add />
-        </IconButton>
-
-        <IconButton onClick={zoomOut}>
-          <RemoveIcon />
-        </IconButton>
-      </Box>
+                <IconButton onClick={() => zoomOut()}>
+                  <RemoveIcon />
+                </IconButton>
+              </Box>
+            </div>
+          )}
+        </TransformWrapper>
+      </div>
     </Box>
   );
 };
